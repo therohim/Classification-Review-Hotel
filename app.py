e7b2d23c4e1d37c1a331add387c17f5dbd939ac9
Nur Rohim <nur.rohim@gmedia.co.id>
2022-01-01 23:58:41 +0700

    first commit

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 from flask import Flask,render_template,url_for,request
 import pandas as pd 
 import pickle
+import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.naive_bayes import MultinomialNB
 # from sklearn.externals import joblib
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
 
 
 app = Flask(__name__)
@@ -14,39 +18,28 @@ def home():
 
 @app.route('/predict',methods=['POST'])
 def predict():
-	df= pd.read_csv("spam.csv", encoding="latin-1")
-	df.drop(['Unnamed: 2', 'Unnamed: 3', 'Unnamed: 4'], axis=1, inplace=True)
+	df= pd.read_csv("review2.csv", encoding="latin-1")
+
 	# Features and Labels
-	df['label'] = df['type'].map({'ham': 0, 'spam': 1})
+	df['label'] = df['type'].map({'positif': 0, 'negatif': 1})
 	X = df['text']
 	y = df['label']
-	
+
 	# Extract Feature With CountVectorizer
 	# Extract Feature With CountVectorizer :cleaning involved converting all of our data to lower case and removing all punctuation marks. 
 	cv = CountVectorizer()
-	X = cv.fit_transform(X) # Fit the Data
-	from sklearn.model_selection import train_test_split
-	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=42)
-	#Naive Bayes Classifier
-	from sklearn.naive_bayes import MultinomialNB
-#particular classifier is suitable for classification with discrete features ( word counts for text classification). It takes in integer word counts as its input. 
+	X = cv.fit_transform(X) 
+	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_state=0)
 	clf = MultinomialNB() #NAIVE BAYES
 	clf.fit(X_train,y_train)
 	clf.score(X_test,y_test)
 	
-
-
-
-
-
 	if request.method == 'POST':
 		message = request.form['message']
 		data = [message]
 		vect = cv.transform(data).toarray()
 		my_prediction = clf.predict(vect)
 	return render_template('result.html',prediction = my_prediction)
-
-
 
 if __name__ == '__main__':
 	app.run(debug=True)
